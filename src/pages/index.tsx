846d090d5a121fbab3eac6713bc41c0dcee19950
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-02-14 22:12:30 +0100

    add landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,78 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import { Container } from "../components/Container/Container";
-import { Link } from "nerdux-ui-system";
-
+import { Link, Switch, TextField, Checkbox, Button } from "nerdux-ui-system";
+import Gameboys from "../../public/gameboys.png";
 import * as styles from "./index.module.scss";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main>
-      <Container>
+    <Container>
+      <aside className={styles.container__left}>
+        <img className={styles.gameboysUp} src={Gameboys} alt="gameboys" />
+      </aside>
+      <section className={styles.container__center}>
         <header className={styles.titleHeader}>
-          <h1>Lead generator project</h1>
-          <Link
-            to={"https://nerdux.nerdbord.io/?path=/story/inputs-button--button"}
-            target={"_blank"}
-          >
-            Open UI components documentation
-          </Link>
+          <h1>
+            Join the Gameboy
+            <br />
+            <span>waiting list</span>
+          </h1>
         </header>
-      </Container>
-    </main>
+        <main>
+          <form>
+            <div className={styles.switch__container}>
+              <p>I swear, I’m a classic gameboy fan</p>
+              <Switch
+                onChange={() => {}}
+                id="switch-1"
+                checked={true}
+                disabled={false}
+              />
+            </div>
+            <div className={styles.textfield__container}>
+              <div className={styles.textfield__containerItem}>
+                <TextField
+                  value=""
+                  onChange={() => {}}
+                  id="huj"
+                  name="username"
+                  label="Name"
+                  placeholder="e.g. Richard Parker"
+                />
+              </div>
+              <TextField
+                value=""
+                onChange={() => {}}
+                id="email-input"
+                name="email"
+                label="Email"
+                placeholder="e.g. richard@gmail.com"
+              />
+            </div>
+            <div className={styles.checkbox__container}>
+              <Checkbox
+                id="checkbox-1"
+                name="checkbox"
+                onChange={() => {}}
+                checked={true}
+                label="I have read and accept the"
+              />
+              <div className={styles.checkbox__containerLink}>
+                <Link to="#"> privacy policy</Link>
+              </div>
+            </div>
+            <Button onClick={() => {}}>Sign me up!</Button>
+          </form>
+        </main>
+      </section>
+      <aside className={styles.container__right}>
+        <img className={styles.gameboysDown} src={Gameboys} alt="gameboys" />
+        <img className={styles.gameboysUp} src={Gameboys} alt="gameboys" />
+        <img className={styles.gameboysDown} src={Gameboys} alt="gameboys" />
+        <img className={styles.gameboysUp} src={Gameboys} alt="gameboys" />
+      </aside>
+    </Container>
   );
 };
 
